f02dea5b8d1c1d2dffe8f27ea593b87ddf3eeede
Minh1509 <nguyenquangminh15092003@gmail.com>
2025-01-19 10:53:09 +0700

    Add route /

--- src/app.js
+++ src/app.js
@@ -25,6 +25,9 @@ checkOverLoad();
 // init routes
 app.use(require("./routes/index"));
 
+app.get("/", (req, res) => {
+  res.send(Hello);
+})
 // handing errors
 app.use((req, res, next) => {
   const error = new Error("Not Found");
